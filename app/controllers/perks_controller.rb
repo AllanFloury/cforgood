625212705faafc5b6d1247d7f33311b04ee06d93
allan floury <allan.floury@gmail.com>
2015-04-29 16:30:12 +0200

    sign in/up + redirection automatique + modifs css sidebar

--- app/controllers/perks_controller.rb
+++ app/controllers/perks_controller.rb
@@ -12,18 +12,4 @@ class PerksController < ApplicationController
   def show
   end
 
-  def create
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
 end
