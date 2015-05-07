72de1de010a94a2524a89e238523855996bc009e
allan floury <allan.floury@gmail.com>
2015-05-07 17:22:04 +0200

    Trying to fix fb on heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]  # Fake password for validation
       user.name = auth.info.name
-      user.picture = auth.info.image.gsub('­http', 'htt­ps')
+      user.picture = auth.info.image#.gsub('­http', 'htt­ps')
       user.token = auth.credentials.token
       user.token_expiry = Time.at(auth.credentials.expires_at)
     end
